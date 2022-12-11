6e7fe59a12cbcde608dd68de71540aad6d348138
theZMC <zach@zmc.dev>
2022-12-10 22:28:12 -0600

    posts: headers have 🔗s now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
 import { defineConfig } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    extendDefaultPlugins: true,
+    rehypePlugins: [
+      rehypeSlug,
+      [rehypeAutolinkHeadings, {
+        behavior: 'wrap',
+        properties: {
+          ariaHidden: true,
+          tabIndex: -1,
+          className: 'anchor'
+        }
+      }]
+    ]
+  },
   site: 'https://zmc.dev',
   integrations: [image({
     serviceEntryPoint: '@astrojs/image/sharp'
